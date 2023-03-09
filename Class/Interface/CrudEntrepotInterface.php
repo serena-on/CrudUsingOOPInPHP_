9799cba8d7115bb466abaab5411b97062f0892a7
Séréna <onirokouserena15@icloud.com>
2023-03-09 10:28:36 +0100

    Add Entrepot Class

---
+++ Class/Interface/CrudEntrepotInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Class\Interface;
+
+interface CrudEntrepotInterface
+{
+    public function getEntrepots();
+    public function getEntrepot(string $CodEntrepIn);
+    public function createEntrepot(string $CodEntrepIn, string $LibEntrepIn, string $AdrEntrepIn, string $CodLocaIn);
+    public function updateEntrepot(string $CodEntrepIn, string $LibEntrepIn, string $AdrEntrepIn, string $CodLocaIn);
+    public function deleteEntrepot(string $CodEntrepIn);
+    public function getCodEntrep() :string;
+    public function getLibEntrep() :string;
+    public function getAdrEntrep() :string;
+    public function getCodLoca() :string;
+}
