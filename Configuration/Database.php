f185f76fcebf2072dbddd09ad3c18852b27d2508
Séréna <onirokouserena15@icloud.com>
2023-03-09 09:19:33 +0100

    first Commit

---
+++ Configuration/Database.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Configuration;
+
+use Class\Exception\FailedToLoginException;
+
+class Database
+{
+    private $host = 'localhost';
+    private $dataBaseName = 'AGRI_DIGITAL';
+    private $dataBaseUser = 'root';
+    private $dataBasePassword = '';
+    private $dsn;
+    private $connection;
+
+    public function __construct()   
+    {
+        try {
+            $this->dsn = "mysql:host=" . $this->host . ";dbname=" . $this->dataBaseName;
+            $this->connection = new \PDO($this->dsn, $this->dataBaseUser, $this->dataBasePassword);
+            $this->connection -> exec("SET NAMES utf8");
+            $this->connection -> setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (FailedToLoginException $e) {
+            echo "Database could not be connected: " . $e->getMessage();
+        }
+    }
+    public function getConnection()
+    {
+        return $this->connection;
+    }
+}
