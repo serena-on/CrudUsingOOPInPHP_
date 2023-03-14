f244ef94210a970f73088b1e265f2082b23f99fb
Séréna <onirokouserena15@icloud.com>
2023-03-14 11:29:48 +0100

    Implement Create Entrepot

---
+++ back/Api/CreateEntrepot.php
@@ -0,0 +1,44 @@
+<?php
+
+    require_once('../vendor/autoload.php');
+
+    use Exception\EmptydataException;
+    use Class\Entrepot;
+    use Configuration\Database;
+
+    header("Access-Control-Allow-Origin: *");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Methods: POST");
+    header("Access-Control-Max-Age: 3600");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+   
+function createEntrepot_() {
+    $database = new Database();
+    $Entrepot = new Entrepot( $database);
+    $data = json_decode(json_encode($_POST));
+    try {
+        if (!isset($data->CodEntrep) && !isset($data->LibEntrep) && !isset($data->AdrEntrep) && !isset($data->CodLoca) ) {
+            throw new EmptydataException();
+         }
+         else {
+            try{
+                if($Entrepot->createEntrepot( CodEntrepIn: $data->CodEntrep, LibEntrepIn: $data->LibEntrep, AdrEntrepIn: $data->AdrEntrep,  CodLocaIn: $data->CodLoca)){
+                    http_response_code(201);
+                    echo json_encode(array("message" => "Entrepot created"));
+                } else{
+                    http_response_code(503);
+                    echo json_encode(array("message" => "Entrepot could not be created."));
+                }                   
+            }
+            catch(Exception $e){
+                http_response_code(503);
+                echo json_encode(array("message" => $e->getMessage()));
+            }
+        }
+    } catch (EmptydataException $e) {
+        echo json_encode(array("message" => $e->getMessage()));
+    }
+}
+
+// var_dump($_POST);
+createEntrepot_();
